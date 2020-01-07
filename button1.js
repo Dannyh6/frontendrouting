ad61b75b826bbb8da1a6d25603474970808de51e
Dannyh6 <Dannyh6@uci.edu>
2020-01-06 21:15:26 -0800

    createpagewithnavi

---
+++ button1.js
@@ -0,0 +1,14 @@
+
+
+//ready function//
+
+//when a button is clicked//
+//do something//
+console.log("javascriptisworking")
+$(document).ready(function() 
+{
+    $("#button1").on("click", function()
+    {
+        alert("alert")
+    })
+})
